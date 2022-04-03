e4357c020d8070cc3c57a0454be9ab0c8ae29ecc
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-04-03 12:07:18 +0700

    refactor(main): [version-1] move hnadler function to package handler

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"encoding/json"
+	"golangWeb/handler"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-type student struct {
-	ID    int
-	Name  string
-	Grade int
-}
-
-var data = []student{
-	{1, "Adri", 6},
-	{2, "Tono", 1},
-	{3, "Setyawan", 2},
-	{4, "Fathur", 3},
-	{5, "12", 6},
-}
 
 func main() {
 	mux := http.NewServeMux()
 	// Declaration routing with func
-	mux.HandleFunc("/", rootHandler)
-	mux.HandleFunc("/hello", helloHandler)
-	mux.HandleFunc("/student", studentHandler)
-	mux.HandleFunc("/students", studentsHandler)
+	mux.HandleFunc("/", handler.RootHandler)
+	mux.HandleFunc("/hello", handler.HelloHandler)
+	mux.HandleFunc("/student", handler.StudentHandler)
+	mux.HandleFunc("/studentDisplay", handler.StudentDisplayHandler)
+	mux.HandleFunc("/students", handler.StudentsHandler)
 	// Declaration routing with closure
 	mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Profile"))
@@ -36,62 +22,4 @@ func main() {
 
 	err := http.ListenAndServe(":3000", mux)
 	log.Fatal(err)
-}
-
-func studentHandler(w http.ResponseWriter, r *http.Request) {
-	var id = r.URL.Query().Get("id")
-	var result []byte
-	var err error
-	// Checking Id is string or not
-	idNumb, err := strconv.Atoi(id)
-	if err != nil {
-		http.Error(w, "String is not allowed", http.StatusNotFound)
-		return
-	}
-	for _, each := range data {
-		if each.ID == idNumb {
-			result, err = json.Marshal(each)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			w.Write(result)
-			return
-		}
-	}
-
-	http.Error(w, "User not found", http.StatusNotFound)
-	return
-}
-
-func studentsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method == "GET" {
-		var result, err = json.Marshal(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(result)
-		return
-	}
-
-	http.Error(w, "", http.StatusBadRequest)
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	log.Println(r.URL)
-	w.Write([]byte("Welcome To Home"))
-}
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	w.Write([]byte("Hello Saya baru coba golang website"))
 }
